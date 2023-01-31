c4e013b8fa63c36a02f6e358721f17707fce6c6e
Petro Lehtonen <petro.lehtonen@gmail.com>
2023-01-31 11:50:32 +0200

    rotating left and right

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -10,4 +10,23 @@ export class RotatingShape {
         let s = this.shape.join("\n") + "\n";
         return s;
     }
+
+    rotateRight() {
+        let s = "";
+        for (let col = 0; col < this.shape.length; col++) {
+            for (let row = this.shape.length - 1; row >= 0; row--) {
+                s += this.shape[row].charAt(col);
+            }
+            if (col !== this.shape.length - 1) {
+                s += "\n";
+            }
+        }
+        return new RotatingShape(s)
+    }
+
+    rotateLeft() {
+        return this.rotateRight()
+                    .rotateRight()
+                    .rotateRight();
+    }
 }
