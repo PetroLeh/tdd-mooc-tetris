cd6cea69b44a36d36106e6c6d0453e1d1413db69
Petro Lehtonen <petro.lehtonen@gmail.com>
2023-01-31 10:44:32 +0200

    refactoring

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,52 +1,68 @@
 export class Board {
+  EMPTY = ".";
   width;
   height;
-  board;
-  falling;
   fallingBlock;
   fallingOnRow;
+  fallingOnCol;
+  board;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.board = ["...", "...", "..."]
-    this.falling = false;
+    this.fallingBlock = null;
+    this.board = this.initEmptyBoard();
+  }
+
+  initEmptyBoard() {
+    let board = new Array(this.height);
+    for (let i = 0; i < this.height; i++) {
+        let row = new Array(this.width);
+        row.fill(this.EMPTY);
+        board[i] = row;
+    }
+    return board;
   }
 
   toString() {
     let s = "";
-    for (let row = 0; row < this.board.length; row++) {
-        s += this.board[row] + "\n";
+    for (let row = 0; row < this.height; row++) {
+        for (let col = 0; col < this.width; col++) {
+            if (this.fallingBlock !== null && row === this.fallingOnRow && col === this.fallingOnCol) {
+                s += this.fallingBlock.color;
+            } else {
+                s += this.board[row][col];
+            }
+        }
+        s += "\n";
     }
     return s;
   }
 
   drop(block) {
-    if(this.falling) {
-        throw("already falling")
+    if(this.fallingBlock !== null) {
+        throw("A block is already falling")
     }
+    this.fallingBlock = block;
     this.fallingOnRow = 0;
-    this.fallingBlock = block
-    this.board[this.fallingOnRow] = "." + this.fallingBlock.color + ".";
-    this.falling = true;
+    this.fallingOnCol = 1;
   }
 
   tick() {
+    let row = this.fallingOnRow;
+    let col = this.fallingOnCol;
     if (this.hasFalling()) {
-        if (this.fallingOnRow < this.board.length - 1 && this.board[this.fallingOnRow + 1] === "...") {
-            this.board[this.fallingOnRow] = "...";
+        if (row < this.height - 1 && this.board[row + 1][col] === this.EMPTY) {
             this.fallingOnRow++;
-            this.board[this.fallingOnRow] = "." + this.fallingBlock.color + ".";
         } else {
-            this.falling = false;
-            return;
+            this.board[row][col] = this.fallingBlock.color;
+            this.fallingBlock = null;
+            this.fallingOnRow = 0;
         }
     }
-
-
   }
 
   hasFalling() {
-    return this.falling;
+    return this.fallingBlock !== null;
   }
 }
