6641d0a1335f82c2620ed04c13a22046d3d9d267
Petro Lehtonen <petro.lehtonen@gmail.com>
2023-01-31 12:02:00 +0200

    Tetromino first test to pass

---
+++ src/Tetromino.mjs
@@ -0,0 +1,12 @@
+import { RotatingShape } from "./RotatingShape.mjs";
+
+export class Tetromino {
+
+    static T_SHAPE = new RotatingShape(
+        `.T.
+         TTT
+         ...`
+    );
+
+
+}
