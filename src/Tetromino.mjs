e88301dbda9d5d18e18982bf12aa45944a2468e2
Petro Lehtonen <petro.lehtonen@gmail.com>
2023-02-09 09:16:45 +0200

    refactoring

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -5,7 +5,7 @@ export class Tetromino {
     static T_SHAPE = new Tetromino(
         `.T.
          TTT
-         ...`
+         ...`, 4
     );
 
     static I_SHAPE = new Tetromino(
@@ -13,27 +13,39 @@ export class Tetromino {
          .....
          IIII.
          .....
-         .....`
+         .....`, 2
     )
 
     piece;
+    orientations;
+    currentOrientation;
     
-    constructor (shape) {
+    constructor (shape, orientations, currentOrientation = 0) {
         this.piece = new RotatingShape(shape)
+        this.orientations = [];
+        this.currentOrientation = currentOrientation;
+
+        let orientation = this.piece;
+        for (let i = 0; i < orientations; i++)  {
+            this.orientations.push(orientation)
+            orientation = orientation.rotateRight();
+        }
     }
 
     rotateRight() {
-        let newShape = this.piece.rotateRight().getShape();
-        return new Tetromino(newShape);
+        let newOrientation = this.currentOrientation + 1;
+        if (newOrientation >= this.orientations.length) newOrientation = 0;
+        return new Tetromino(this.piece.getShape(), this.orientations.length, newOrientation);
     }
 
     rotateLeft() {
-        let newShape = this.piece.rotateLeft().getShape();
-        return new Tetromino(newShape);
+        let newOrientation = this.currentOrientation - 1;
+        if (newOrientation < 0) newOrientation = this.orientations.length - 1;
+        return new Tetromino(this.piece.getShape(), this.orientations.length, newOrientation);
     }
 
     toString() {
-        return this.piece.toString();
+        return this.orientations[this.currentOrientation].toString();
     }
 
 }
